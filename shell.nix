9c7700998112bd55dca1661cf4857a592de6c4e4
Dilip <igoldlip@gmail.com>
2026-09-06 18:32:17 +0530

    init: dev setup to get started
    * has all batteries to go ahead

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  name = "fread-dev";
+
+  buildInputs = with pkgs; [
+    python314
+    uv
+    just
+    git
+  ];
+
+  shellHook = ''
+    if ! command -v fm >/dev/null 2>&1; then
+      echo "fm (frappe-manager) not found -- install once with: uv tool install frappe-manager"
+    fi
+  '';
+}
